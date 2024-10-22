baf5624cbe9bd2c0c5bcd43a8306c92ef89cfa09
wael <wael.slama.issat@gmail.com>
2024-10-22 16:47:11 +0100

    first commit

---
+++ lib/homePage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Welcome to Homepage',style: TextStyle(fontSize: 24),),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/chatPage.dart';
+import 'package:flutter_application_1/homePage.dart';
+import 'package:flutter_application_1/profilePage.dart';
+import 'package:flutter_application_1/searchPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Tp2",
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+          useMaterial3: true),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _page = 0;
+  List<Widget> list = [
+    HomePage(),
+    SearchPage(),
+    ChatPage(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("home Page"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+       body: list[_page],
+      // body: IndexedStack(
+      //   index: _page,
+      //   children: list,
+      //),
+      // body: Container(),
+      bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.blueAccent,
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
+          // onTap: (index) {
+
+          //   if (index == 0) {
+          //   Navigator.push(context,
+          //   MaterialPageRoute(builder: (context) => HomePage()));
+          //   } else if (index == 1) {
+          //   Navigator.push(context,
+          //   MaterialPageRoute(builder: (context) => SearchPage()));
+          //   } else if (index == 2) {
+          //   Navigator.push(context,
+          //   MaterialPageRoute(builder: (context) => ChatPage()));
+          //   } else {
+          //   Navigator.push(context,
+          //   MaterialPageRoute(builder: (context) => ProfilePage()));
+          //   }
+          //   },
+          items: [
+            CurvedNavigationBarItem(child: Icon(Icons.home), label: "Home"),
+            CurvedNavigationBarItem(child: Icon(Icons.search), label: "Search"),
+            CurvedNavigationBarItem(child: Icon(Icons.message), label: "Chat"),
+            CurvedNavigationBarItem(
+                child: Icon(Icons.perm_identity), label: "Profile"),
+          ]),
+    );
+  }
+}
